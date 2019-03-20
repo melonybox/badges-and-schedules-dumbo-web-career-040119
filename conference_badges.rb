faaff8c030ea460826ee87ccf0d1c6043c2a4c4e
Gino Desangles <blushingbox@yahoo.com>
2019-03-20 03:17:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,12 +5,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(list)
-  i = 0
-  maxI = list.size
   newList = []
-  until i == maxI
-    newList.push(`Hello, my name is #{list[i]}.`)
-    i += 1
-  end
-  return newList
+  list.each do |x|
+    
 end
